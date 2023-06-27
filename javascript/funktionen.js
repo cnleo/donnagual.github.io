4f41c2d3c81bf92fed8a96e95ed586ce1945212c
DonNagual <100430787+DonNagual@users.noreply.github.com>
2023-06-27 12:41:06 +0200

    Add files via upload

---
+++ javascript/funktionen.js
@@ -0,0 +1,25 @@
+
+
+function showNav() {
+    document.getElementById("navBar").style.height = "50px";
+}
+function hideNav() {
+    document.getElementById("navBar").style.height = "0";
+}
+
+function showdropGalerie() {    
+    document.getElementById("dropGalerie").style.display = "flex";
+}
+function hidedropGalerie() {
+    document.getElementById("dropGalerie").style.display = "none";
+}
+
+function showdropProjekt() {    
+    document.getElementById("dropProjekt").style.display = "flex";
+}
+function hidedropProjekt() {
+    document.getElementById("dropProjekt").style.display = "none";
+}
+
+
+
